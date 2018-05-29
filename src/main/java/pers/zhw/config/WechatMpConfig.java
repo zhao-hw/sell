93b89bcc15974dbfe9feb448f3fc5c2d0dfb998a
hw.zhao <hw.zhao@outlook.com>
2018-05-29 17:14:57 +0800

    增加微信网页授权,获取openid

---
+++ src/main/java/pers/zhw/config/WechatMpConfig.java
@@ -0,0 +1,31 @@
+package pers.zhw.config;
+
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WechatMpConfig {
+
+    @Autowired
+    private WechatAccountConfig accountConfig;
+
+    @Bean
+    public WxMpService wxMpService() {
+        WxMpService wxMpService = new WxMpServiceImpl();
+        wxMpService.setWxMpConfigStorage(wxMpInMemoryConfigStorage());
+        return wxMpService;
+    }
+
+    @Bean
+    public WxMpInMemoryConfigStorage wxMpInMemoryConfigStorage() {
+        WxMpInMemoryConfigStorage wxMpInMemoryConfigStorage = new WxMpInMemoryConfigStorage();
+        wxMpInMemoryConfigStorage.setAppId(accountConfig.getMpAppId());
+        wxMpInMemoryConfigStorage.setSecret(accountConfig.getMpAppSecret());
+        return wxMpInMemoryConfigStorage;
+    }
+}
