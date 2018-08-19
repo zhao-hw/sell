570e57e718acce524f8ce68079419ff54f1d9a2c
hw.zhao <hw.zhao@outlook.com>
2018-08-19 13:45:58 +0800

    product增加表单验证

--- src/main/java/pers/zhw/form/ProductForm.java
+++ src/main/java/pers/zhw/form/ProductForm.java
@@ -2,20 +2,27 @@ package pers.zhw.form;
 
 import lombok.Data;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 @Data
 public class ProductForm {
     private String productId;
     /** 名字. */
+    @NotNull
     private String productName;
     /** 单价. */
+    @NotNull
     private BigDecimal productPrice;
     /** 库存. */
+    @NotNull
     private Integer productStock;
     /** 描述. */
+    @NotNull
     private String productDescription;
     /** 小图. */
     private String productIcon;
     /** 类目编号. */
+    @NotNull
     private Integer categoryType;
 }
